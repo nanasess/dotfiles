e3b396fcaec757947b3b71ff4ba86c18c2c4eda9
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2010-09-30 22:14:59 +0900

    * init.el (ac-auto-start): Removed.

--- init.el
+++ init.el
@@ -422,7 +422,6 @@
 (global-auto-complete-mode t)
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
-(setq ac-auto-start nil)
 (global-set-key "\M-/" 'ac-start)
 
 (set-default 'ac-sources '(ac-source-yasnippet
