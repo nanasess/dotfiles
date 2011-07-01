89ac52497643bd0d3ca0ecf86bf4dd1bb16c8c14
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2011-07-01 11:02:24 +0900

    * init.el: scroll-bar-mode => 1, linum-mode => 0

--- init.el
+++ init.el
@@ -171,7 +171,6 @@
 ;;;
 
 (cond (window-system
-       (scroll-bar-mode 0)
        (tool-bar-mode 0)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -203,7 +202,6 @@
 ;;;
 
 (defun basic-indent ()
-  (linum-mode 1)
   (setq tab-width 4)
   (setq indent-tabs-mode nil))
 
@@ -214,7 +212,6 @@
 
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
-	    (linum-mode 1)
 	    (setq tab-width 8)
 	    (setq indent-tabs-mode t)))
 
@@ -257,7 +254,6 @@
 	     '("\\.\\(xml\\|xsl\\|rng\\|html\\|tpl\\)\\'" . nxml-mode))
 (add-hook 'nxml-mode-hook
 	  (lambda ()
-	    (linum-mode 1)
 	    (setq auto-fill-mode -1)
 	    (setq nxml-slash-auto-complete-flag t)
 	    (setq nxml-child-indent 2)
@@ -400,7 +396,7 @@
     :delimiter-mode nil)))
 (mmm-add-mode-ext-class nil "\\.tpl?\\'" 'embedded-css)
 (mmm-add-mode-ext-class nil "\\.tpl?\\'" 'embedded-js)
-(mmm-add-mode-ext-class nil "\\.php\\'" 'php-others)
+;; (mmm-add-mode-ext-class nil "\\.php\\'" 'php-others)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -561,7 +557,6 @@
 (add-hook 'php-mode-hook
 	  (lambda ()
 	    (gtags-mode 1)
-	    (linum-mode 1)
 	    (require 'php-completion)
 	    (php-completion-mode t)
 	    (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
