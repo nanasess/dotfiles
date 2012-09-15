8a0383bf7591edf67d64960942cc424c6996be0f
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2012-09-16 01:01:12 +0900

    Improved `php-mode'.

--- init.el
+++ init.el
@@ -223,30 +223,6 @@
 (when (<= emacs-major-version 23)
   (add-to-list 'load-path (expand-file-name (concat user-site-lisp-directory "cc"))))
 
-;; Create my personal style.
-(defconst php-style
-  '((c-tab-always-indent        . t)
-    (c-comment-only-line-offset . 0)
-    (c-hanging-braces-alist     . ((substatement-open after)
-                                   (brace-list-open)))
-    (c-hanging-colons-alist     . ((member-init-intro before)
-                                   (inher-intro)
-                                   (case-label after)
-                                   (label after)
-                                   (access-label after)))
-    (c-cleanup-list             . (scope-operator
-                                   empty-defun-braces
-                                   defun-close-semi))
-    (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
-                                   (substatement-open . 0)
-                                   (case-label        . 4)
-                                   (block-open        . 0)
-                                   (knr-argdecl-intro . -)))
-    (c-echo-syntactic-information-p . t))
-  "My PHP Programming Style")
-
-(c-add-style "php-style" php-style)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Indent settings
@@ -607,24 +583,98 @@
 ;;;
 
 (require 'php-mode)
+
+(defconst php-style
+  `((c-recognize-knr-p . nil)
+    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
+    (c-basic-offset . 4)
+    (indent-tabs-mode . nil)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((defun-open after)
+                               (defun-close before after)
+                               (class-open after)
+                               (class-close before after)
+                               (namespace-open after)
+                               (inline-open after)
+                               (inline-close before after)
+                               (block-open after)
+                               (block-close . c-snug-do-while)
+                               (extern-lang-open after)
+                               (extern-lang-close after)
+                               (statement-case-open after)
+                               (substatement-open after)))
+    (c-hanging-colons-alist . ((case-label)
+                               (label after)
+                               (access-label after)
+                               (member-init-intro before)
+                               (inher-intro)))
+    (c-hanging-semi&comma-criteria
+     . (c-semi&comma-no-newlines-for-oneline-inliners
+        c-semi&comma-inside-parenlist
+        c-semi&comma-no-newlines-before-nonblanks))
+    (c-indent-comments-syntactically-p . nil)
+    (comment-column . 40)
+    (c-cleanup-list . (brace-else-brace
+                       brace-elseif-brace
+                       brace-catch-brace
+                       empty-defun-braces
+                       defun-close-semi
+                       list-close-comma
+                       scope-operator))
+    (c-offsets-alist . ((func-decl-cont . ++)
+                        (member-init-intro . ++)
+                        (inher-intro . ++)
+                        (comment-intro . 0)
+                        (arglist-close . c-lineup-arglist)
+                        (topmost-intro . 0)
+                        (block-open . 0)
+                        (inline-open . 0)
+                        (substatement-open . 0)
+                        (statement-cont
+                         .
+                         (,(when (fboundp 'c-no-indent-after-java-annotations)
+                             'c-no-indent-after-java-annotations)
+                          ,(when (fboundp 'c-lineup-assignments)
+                             'c-lineup-assignments)
+                          ++))
+                        (label . /)
+                        (case-label . +)
+                        (statement-case-open . +)
+                        (statement-case-intro . +) ; case w/o {
+                        (access-label . /)
+                        (innamespace . 0))))
+  "
+My PHP Programming Style
+see http://google-styleguide.googlecode.com/svn/trunk/google-c-style.el")
+
+(defun php-c-style ()
+  (interactive)
+  (make-local-variable 'c-tab-always-indent)
+  (setq c-tab-always-indent t)
+  (gtags-mode 1)
+  (electric-pair-mode 1)
+  (electric-indent-mode 1)
+  (electric-layout-mode 1)
+  (c-toggle-auto-hungry-state 1)
+  ;; (make-variable-buffer-local 'electric-layout-rules) '((?\{ . after)
+  ;; 							(?\; . after))
+  (require 'php-completion)
+  (php-completion-mode t)
+  (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
+  (when (require 'auto-complete nil t)
+    (make-variable-buffer-local 'ac-sources)
+    (add-to-list 'ac-sources
+		 'ac-source-php-completion
+		 'ac-source-yasnippet)
+    (auto-complete-mode t))
+  (c-add-style "php-style" php-style t))
+
 (add-to-list 'auto-mode-alist '("\\.\\(inc\\|php[s34]?\\)" . php-mode))
 (setq php-mode-force-pear t)
 (setq php-manual-url "http://jp2.php.net/manual/ja/")
 (setq php-search-url "http://jp2.php.net/")
-(add-hook 'php-mode-hook
-	  (lambda ()
-	    (gtags-mode 1)
-	    (require 'php-completion)
-	    (php-completion-mode t)
-	    (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
-	    (c-set-style "php-style")
-	    (setq c-auto-newline t)
-	    (when (require 'auto-complete nil t)
-	      (make-variable-buffer-local 'ac-sources)
-	      (add-to-list 'ac-sources
-			   'ac-source-php-completion
-			   'ac-source-yasnippet)
-	      (auto-complete-mode t))))
+
+(add-hook 'php-mode-hook 'php-c-style)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
