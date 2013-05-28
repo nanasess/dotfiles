13b901298e16f6344916537b1a7294efb0f62933
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2013-05-28 11:28:32 +0900

    `migemo-coding-system' set to the utf-8.

--- init.el
+++ init.el
@@ -359,17 +359,17 @@
 ;;;
 
 (el-get 'sync 'migemo)
-(setq migemo-dictionary "/usr/local/share/migemo/euc-jp/migemo-dict")
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
 (setq isearch-lax-whitespace nil)
 (when (file-exists-p migemo-dictionary)
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs" "-i" "\a"))
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
-  ;; (setq migemo-use-pattern-alist t)
-  ;; (setq migemo-use-frequent-pattern-alist t)
-  ;; (setq migemo-pattern-alist-length 1000)
-  ;; (setq migemo-coding-system 'utf-8-unix)
+  (setq migemo-use-pattern-alist t)
+  (setq migemo-use-frequent-pattern-alist t)
+  (setq migemo-pattern-alist-length 1000)
+  (setq migemo-coding-system 'utf-8-unix)
   (require 'migemo))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1012,11 +1012,11 @@ see http://google-styleguide.googlecode.com/svn/trunk/google-c-style.el")
 (global-set-key (kbd "C-z C-r") 'helm-resume)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
+;; (setq hh:use-migemo t)
 (require 'helm-config)
 (require 'helm-match-plugin)
 (require 'helm-migemo)
 (require 'helm-howm)
-(setq hh:use-migemo t)
 
 (setq hh:menu-list
       '(("m [メニュー]" . "(howm-menu)")
