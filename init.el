6b627e898d9eecc44f50a5c51ddce6afedc985e2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2013-09-28 13:27:34 +0900

    キーボードが壊れたので...

--- init.el
+++ init.el
@@ -127,6 +127,10 @@
 (global-set-key (kbd "C-z C-u") 'other-frame)
 (global-set-key (kbd "C-z C-j") 'toggle-skk-kutouten)
 
+(global-set-key (kbd "<M-up>") 'beginning-of-buffer)
+(global-set-key (kbd "<M-down>") 'end-of-buffer)
+(global-set-key (kbd "C-o") 'previous-line)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; backup files settings
@@ -1305,15 +1309,3 @@ username ALL=NOPASSWD: /opt/local/apache2/bin/apachectl configtest,\\
 (autoload 'po-find-file-coding-system "po-compat")
 (modify-coding-system-alist 'file "\\.po\\'\\|\\.po\\."
 			    'po-find-file-coding-system)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
