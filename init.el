146908e30c12f58fe8e70e619e54f08a9c27380f
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-08-22 14:28:56 +0900

    use `php-mode-coding-style' to `psr2'.

--- init.el
+++ init.el
@@ -717,92 +717,19 @@
 ;;;
 
 (el-get 'sync 'php-mode)
-
-(defconst php-style
-  `((c-recognize-knr-p . nil)
-    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
-    (c-basic-offset . 4)
-    (indent-tabs-mode . nil)
-    (c-comment-only-line-offset . 0)
-    (c-hanging-braces-alist . ((defun-open after)
-                               (defun-close before after)
-                               (class-open after)
-                               (class-close before after)
-                               (namespace-open after)
-                               (inline-open after)
-                               (inline-close before after)
-                               (block-open after)
-                               (block-close . c-snug-do-while)
-                               (extern-lang-open after)
-                               (extern-lang-close after)
-                               (statement-case-open after)
-                               (substatement-open after)))
-    (c-hanging-colons-alist . ((case-label)
-                               (label after)
-                               (access-label after)
-                               (member-init-intro before)
-                               (inher-intro)))
-    (c-hanging-semi&comma-criteria
-     . (c-semi&comma-no-newlines-for-oneline-inliners
-        c-semi&comma-inside-parenlist
-        c-semi&comma-no-newlines-before-nonblanks))
-    (c-indent-comments-syntactically-p . nil)
-    (comment-column . 40)
-    (c-cleanup-list . (brace-else-brace
-                       brace-elseif-brace
-                       brace-catch-brace
-                       empty-defun-braces
-                       defun-close-semi
-                       list-close-comma
-                       scope-operator))
-    (c-offsets-alist . ((func-decl-cont . ++)
-                        (member-init-intro . ++)
-                        (inher-intro . ++)
-                        (comment-intro . 0)
-                        (arglist-close . c-lineup-arglist)
-                        (topmost-intro . 0)
-                        (block-open . 0)
-                        (inline-open . 0)
-                        (substatement-open . 0)
-                        (statement-cont
-                         .
-                         (,(when (fboundp 'c-no-indent-after-java-annotations)
-                             'c-no-indent-after-java-annotations)
-                          ,(when (fboundp 'c-lineup-assignments)
-                             'c-lineup-assignments)
-                          ++))
-                        (label . /)
-                        (case-label . +)
-                        (statement-case-open . +)
-                        (statement-case-intro . +) ; case w/o {
-                        (access-label . /)
-                        (innamespace . 0))))
-  "
-My PHP Programming Style
-see http://google-styleguide.googlecode.com/svn/trunk/google-c-style.el")
-
 (el-get 'sync 'php-electric)
 (el-get 'sync 'php-completion)
+
 (defun php-c-style ()
   (interactive)
-  ;; (php-electric-mode 1)
   (c-toggle-hungry-state 1)
   ;; (c-toggle-auto-hungry-state 1)
-  ;; (flymake-mode 1)
-  (require 'php-completion)
-  (php-completion-mode t)
   (set (make-local-variable 'comment-start) "// ")
   (set (make-local-variable 'comment-start-skip) "// *")
-  (set (make-local-variable 'comment-end) "")
-  (when (require 'auto-complete nil t)
-    (add-to-list 'ac-sources
-		 'ac-source-php-completion
-		 'ac-source-yasnippet)
-    (auto-complete-mode t))
-  (c-add-style "php-style" php-style t))
+  (set (make-local-variable 'comment-end) ""))
 
 (add-to-list 'auto-mode-alist '("\\.\\(inc\\|php[s34]?\\)" . php-mode))
-(custom-set-variables '(php-mode-force-pear t)
+(custom-set-variables '(php-mode-coding-style 'psr2)
 		      '(php-manual-url "http://jp2.php.net/manual/ja/")
 		      '(php-search-url "http://jp2.php.net/"))
 
@@ -812,12 +739,8 @@ see http://google-styleguide.googlecode.com/svn/trunk/google-c-style.el")
 (eval-after-load "php-mode"
   '(progn
      (setq yas-trigger-key (kbd "<tab>"))
-     (setq c-tab-always-indent t)
-     (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
      (define-key php-mode-map [return] 'newline-and-indent)
-     (define-key php-mode-map (kbd "C-z C-t") 'quickrun)
-     (define-key php-mode-map (kbd "M-p") 'flymake-goto-prev-error)
-     (define-key php-mode-map (kbd "M-n") 'flymake-goto-next-error)))
+     (define-key php-mode-map (kbd "C-z C-t") 'quickrun)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
