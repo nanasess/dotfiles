fb9afa376372849bfbd1be164d390cf22901f5d1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-12-19 13:01:39 +0900

    Fix helm-migemo.
    See also.
    http://rubikitch.com/2014/12/19/helm-migemo/

--- init.el
+++ init.el
@@ -1007,6 +1007,18 @@
 (global-set-key (kbd "C-z .") 'hh:resume)
 (global-set-key (kbd "C-z s") 'helm-howm-do-grep)
 
+(eval-after-load "helm-migemo"
+  '(defun helm-compile-source--candidates-in-buffer (source)
+     (helm-aif (assoc 'candidates-in-buffer source)
+         (append source
+                 `((candidates
+                    . ,(or (cdr it)
+                           (lambda ()
+                             ;; Do not use `source' because other plugins
+                             ;; (such as helm-migemo) may change it
+                             (helm-candidates-in-buffer (helm-get-current-source)))))
+                   (volatile) (match identity)))
+       source)))
 (helm-migemize-command helm-source-kill-ring)
 ;; (helm-migemize-command helm-for-files)
 (helm-migemize-command hh:menu-command)
