801e459f482ae6384d98c22b42e227019f356542
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-01-21 17:17:57 +0900

    Improve skk-init
    - use skk-extra-jisyo-file-list
    - ddskk repository to git

--- init.d/skk-init.el
+++ init.d/skk-init.el
@@ -2,12 +2,31 @@
 ;; dot.skk
 ;;
 
-;; (require 'skk-study)
-;; (setq skk-server-inhibit-startup-server nil)
-;; (setq skk-server-prog "dbskkd-cdb-tcp.sh")
-(setq skk-server-host "localhost")
-(setq skk-server-portnum 1178)
-(setq skk-cdb-large-jisyo (concat external-directory "ddskk/SKK-JISYO.ALL.cdb"))
+(setq skk-cdb-large-jisyo nil)
+(setq skk-large-jisyo (concat external-directory "ddskk/SKK-JISYO.L"))
+
+(setq skk-extra-jisyo-file-list
+      (list
+       ;; XXX external-directory をうまく展開できない
+       '("~/GoogleDrive/share/ddskk/SKK-JISYO.JIS3_4" . euc-jisx0213)
+       '("~/GoogleDrive/share/ddskk/SKK-JISYO.JIS2004" . euc-jisx0213)
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.JIS2"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.assoc"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.edict"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.fullname"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.geo"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.itaiji"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.jinmei"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.law"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.lisp"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.mazegaki"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.okinawa"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.propernoun"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.pubdic+"))
+       (format "%s" (concat external-directory "ddskk/SKK-JISYO.station"))
+       (format "%s" (concat external-directory "ddskk/zipcode/SKK-JISYO.zipcode"))
+       (format "%s" (concat external-directory "ddskk/zipcode/SKK-JISYO.office.zipcode"))))
+
 (setq-default skk-kutouten-type 'en)
 (setq-default skk-kuten-touten-alist
 	      '((jp "。" . "、")
