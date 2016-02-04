7564d37b9b489e3399fa98893e64e1f61afe8c81
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-02-04 11:06:59 +0900

    Modify font settings.

--- init.d/arch/usg-unix-v/init.el
+++ init.d/arch/usg-unix-v/init.el
@@ -1,8 +1,21 @@
-(server-start)
+(create-fontset-from-ascii-font "Circle M+ 1m-12:weight=normal:slant=normal" nil "solaris")
+(set-fontset-font "fontset-solaris" 'unicode
+		  (font-spec :family "Circle M+ 1m" ) nil 'append)
+(set-fontset-font "fontset-solaris" '(#x0080 .  #x024F)
+		  (font-spec :family "Circle M+ 1m") nil 'prepend)
 
-;; grep-find で .svn を除外
-(setq grep-find-command 
-      (cons (concat "find . -type f -name '*.svn*' -prune -o -exec "
-		    "/usr/xpg4/bin/grep -n -e  {} /dev/null \\;") 72))
+;; Window settings
+(if (boundp 'window-system)
+    (setq initial-frame-alist
+	  (append (list
+		   '(width . 82)
+		   '(height . 50)
+		   '(font . "fontset-solaris"))
+		  initial-frame-alist)))
+(setq default-frame-alist initial-frame-alist)
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
 
 (provide 'usg-unix-v-init)
