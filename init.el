dd94e59ccccee4b26786ae37ff2deb417e6f24ad
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-07-24 10:12:55 +0900

    add comments

--- init.el
+++ init.el
@@ -1472,4 +1472,10 @@ username ALL=NOPASSWD: /opt/local/apache2/bin/apachectl configtest,\\
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
