183aa193a887087308d2385b902ca861b96c3cb9
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-08-29 11:49:23 +0900

    Fix font-settings

--- init.d/arch/gnu/linux/init.el
+++ init.d/arch/gnu/linux/init.el
@@ -1,15 +1,17 @@
 ;;----------------------- window settings ---------------------------
-(create-fontset-from-ascii-font "Circle M+ 1m-12:weight=normal:slant=normal" nil "linux")
-(set-fontset-font "fontset-linux" 'unicode
-		  (font-spec :family "Circle M+ 1m" ) nil 'append)
-(set-fontset-font "fontset-linux" '(#x0080 .  #x024F)
-		  (font-spec :family "Circle M+ 1m") nil 'prepend)
 (if (boundp 'window-system)
-    (setq initial-frame-alist
-	  (append (list
-		   '(height . 50)
-		   '(width  . 82)
-		   '(font . "fontset-linux")
-		   initial-frame-alist))))
-(setq default-frame-alist initial-frame-alist)
+    (create-fontset-from-ascii-font "Circle M+ 1m-12:weight=normal:slant=normal" nil "linux")
+  (set-fontset-font "fontset-linux" 'unicode
+		    (font-spec :family "Circle M+ 1m" ) nil 'append)
+  (set-fontset-font "fontset-linux" '(#x0080 .  #x024F)
+		    (font-spec :family "Circle M+ 1m") nil 'prepend)
+
+  (setq initial-frame-alist
+	(append (list
+		 '(height . 50)
+		 '(width  . 82)
+		 '(font . "fontset-linux")
+		 initial-frame-alist)))
+  (setq default-frame-alist initial-frame-alist))
+
 (provide 'linux-init)
