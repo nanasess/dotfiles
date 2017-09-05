95d30adcafac5e150ca527428df7d2156a844fb3
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-09-05 10:52:07 +0900

    Add endless/visit-pull-request-url

--- init.el
+++ init.el
@@ -644,12 +644,47 @@
 ;;;
 
 (el-get-bundle magit
-  (setq magit-diff-refine-hunk t))
+  (setq magit-diff-refine-hunk t)
 
-(with-eval-after-load-feature 'magit
-  (define-key magit-log-mode-map (kbd "j") 'magit-section-forward)
-  (define-key magit-log-mode-map (kbd "k") 'magit-section-backward))
+  ;; see https://stackoverflow.com/a/32914548/4956633
+  (defun endless/visit-pull-request-url ()
+    "Visit the current branch's PR on Github."
+    (interactive)
+    (let ((repo (magit-get "remote" (magit-get-remote) "url")))
+      (if (not repo)
+	  (setq repo (magit-get "remote" (magit-get-push-remote) "url")))
+      (if (string-match "github\\.com" repo)
+	  (visit-gh-pull-request repo)
+	(visit-bb-pull-request repo))))
 
+  (defun visit-gh-pull-request (repo)
+    "Visit the current branch's PR on Github."
+    (interactive)
+    (message repo)
+    (browse-url
+     (format "https://github.com/%s/pull/new/%s"
+	     (replace-regexp-in-string
+	      "\\`.+github\\.com:\\(.+\\)\\(\\.git\\)?\\'" "\\1"
+	      repo)
+	     (magit-get-current-branch))))
+
+  ;; Bitbucket pull requests are kinda funky, it seems to try to just do the
+  ;; right thing, so there's no branches to include.
+  ;; https://bitbucket.org/<username>/<project>/pull-request/new
+  (defun visit-bb-pull-request (repo)
+    (message repo)
+    (browse-url
+     (format "https://bitbucket.org/%s/pull-request/new?source=%s&t=1"
+	     (replace-regexp-in-string
+	      "\\`.+bitbucket\\.org:\\(.+\\)\\.git\\'" "\\1"
+	      repo)
+	     (magit-get-current-branch))))
+
+  (with-eval-after-load-feature 'magit
+    ;; visit PR for github or bitbucket repositories with "v"
+    (define-key magit-mode-map "v" #'endless/visit-pull-request-url)
+    (define-key magit-log-mode-map (kbd "j") 'magit-section-forward)
+    (define-key magit-log-mode-map (kbd "k") 'magit-section-backward)))
 (global-set-key (kbd "C-z m") 'magit-status)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
