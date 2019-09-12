4a82f71a08f3749956e38292790dc8c9ff07baa4
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-12 17:11:04 +0900

    Modify face-attribute to font-lock-*

--- init.el
+++ init.el
@@ -192,6 +192,9 @@
 ;; use solarized.
 (el-get-bundle doom-themes)
 (load-theme 'doom-solarized-light t)
+(set-face-attribute 'font-lock-comment-face nil :slant 'normal)
+(set-face-attribute 'font-lock-type-face nil :slant 'normal :weight 'bold)
+(set-face-attribute 'font-lock-builtin-face nil :slant 'normal :weight 'bold)
 
 (require 'whitespace)
 (setq whitespace-style
