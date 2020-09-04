daecdd807ff3536b0df67f4d22bef6330c6f9879
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-09-04 21:46:53 +0900

    Fix plantuml-indent-level

--- init.el
+++ init.el
@@ -1597,6 +1597,7 @@
   :pkgname "skuro/plantuml-mode"
   (add-to-list 'auto-mode-alist '("\\.puml$" . plantuml-mode))
   (with-eval-after-load-feature 'plantuml-mode
+    (setq plantuml-indent-level 2)
     (setq plantuml-executable-path "plantuml")
     (setq plantuml-default-exec-mode 'executable)
     (setq plantuml-output-type "png")
