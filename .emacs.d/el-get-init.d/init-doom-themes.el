e966ab37398a77fc63ae80825cba3ec1d6a3ecc8
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-06-26 19:10:00 +0900

    Fix face to comments.

--- .emacs.d/el-get-init.d/init-doom-themes.el
+++ .emacs.d/el-get-init.d/init-doom-themes.el
@@ -2,4 +2,7 @@
           #'(lambda ()
               (require 'doom-themes)
               ;; use solarized.
-              (load-theme 'doom-solarized-light t)))
+              (load-theme 'doom-solarized-light t)
+              (set-face-attribute 'font-lock-comment-face nil :slant 'normal)
+              (set-face-attribute 'font-lock-type-face nil :slant 'normal :weight 'bold)
+              (set-face-attribute 'font-lock-builtin-face nil :slant 'normal :weight 'bold)))
