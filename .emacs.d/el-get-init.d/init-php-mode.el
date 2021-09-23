d1a440586fcf0f7275998fa4282fff3131081a37
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-09-24 00:09:02 +0900

    Fix path to composer

--- .emacs.d/el-get-init.d/init-php-mode.el
+++ .emacs.d/el-get-init.d/init-php-mode.el
@@ -10,9 +10,7 @@
     (add-hook 'php-mode-hook
               #'(lambda ()
                   (setq c-auto-newline 1)
-                  (setq c-hungry-delete-key 1))))
-  (with-eval-after-load-feature 'lsp-mode
-    (setq lsp-php-composer-dir "~/.composer")))
+                  (setq c-hungry-delete-key 1)))))
 (with-eval-after-load-feature 'php
   (setq php-manual-url "https://www.php.net/manual/ja/"
         php-mode-coding-style 'Symfony2
