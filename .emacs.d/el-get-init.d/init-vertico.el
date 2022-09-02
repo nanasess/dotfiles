10c84ff102674956ea63ea163aa1294dc5826f11
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-09-02 21:36:47 +0900

    Fix custome-face

--- .emacs.d/el-get-init.d/init-vertico.el
+++ .emacs.d/el-get-init.d/init-vertico.el
@@ -1,11 +1,9 @@
 (add-hook 'after-init-hook
           #'(lambda ()
               (vertico-mode)
-              ;; (fido-vertical-mode +1)
               (marginalia-mode)
               (savehist-mode)))
 (with-eval-after-load 'vertico
-  (custom-declare-face 'vertico-group-title '((t :inherit shadow :slant italic :foreground "#788484")) nil )
   (setq read-file-name-completion-ignore-case t
         read-buffer-completion-ignore-case t
         completion-ignore-case t)
@@ -14,6 +12,7 @@
   (require 'orderless)
   (require 'marginalia)
   (require 'savehist)
+
   (global-set-key (kbd "C-z C-r") #'vertico-repeat)
   (add-hook 'minibuffer-setup-hook #'vertico-repeat-save)
   (define-key vertico-map (kbd "C-l") #'vertico-directory-up)
