016547cf9a043138b9c73d5dc0d990cd777b3612
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2023-07-03 11:17:05 +0900

    Fix tsx-ts-mode

--- .emacs.d/el-get-init.d/init-web-mode.el
+++ .emacs.d/el-get-init.d/init-web-mode.el
@@ -1,5 +1,3 @@
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
@@ -13,14 +11,6 @@
             #'(lambda ()
                 (setq web-mode-enable-auto-indentation nil)))
   (add-hook 'web-mode-hook 'prettier-js-mode)
-  (add-hook 'web-mode-hook
-            #'(lambda ()
-                (when (string-equal "tsx" (file-name-extension buffer-file-name))
-                  (setup-tide-mode))))
-  (add-hook 'web-mode-hook
-            #'(lambda ()
-                (when (string-equal "jsx" (file-name-extension buffer-file-name))
-                  (setup-tide-mode))))
   (add-hook 'web-mode-hook
             #'(lambda ()
                 (when (string-equal "vue" (file-name-extension buffer-file-name))
