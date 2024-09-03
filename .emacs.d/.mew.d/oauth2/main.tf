97149bc8d55ce29d95866a2fcc745a8a81e53846
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-09-03 22:43:12 +0900

    Add mew-oauth2

--- .emacs.d/.mew.d/oauth2/main.tf
+++ .emacs.d/.mew.d/oauth2/main.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
+      version = "~> 4.0.1"
     }
   }
   required_version = ">= 0.14.9"
