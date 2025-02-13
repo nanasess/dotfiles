e96da5e33b708c40dd92349fcd6432367fb64992
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-13 23:12:27 +0900

    fix: update el-get.lock and init.el for emacs-fsharp-mode
    - Added emacs-fsharp-mode to el-get.lock with checksum.
    - Corrected indentation in el-get.lock.
    - Updated init.el to use emacs-fsharp-mode instead of fsharp-mode.
    - Uncommented and configured howm package in init.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -646,9 +646,9 @@
 
 (setopt howm-directory (concat external-directory "howm/"))
 (el-get-bundle howm
-  ;; :type github
-  ;; :pkgname "kaorahi/howm"
-  ;; :build `(("./configure" ,(concat "--with-emacs=" el-get-emacs)) ("make"))
+  :type github
+  :pkgname "kaorahi/howm"
+  :build `(("./configure" ,(concat "--with-emacs=" el-get-emacs)) ("make"))
   :prepare (progn
              (defvar howm-menu-lang 'ja)
              (defvar howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.txt")
@@ -1046,7 +1046,7 @@
   :pkgname "Groovy-Emacs-Modes/groovy-emacs-modes")
 
 (el-get-bundle csv-mode in emacsmirror/csv-mode)
-(el-get-bundle fsharp-mode
+(el-get-bundle emacs-fsharp-mode
   :type github
   :pkgname "fsharp/emacs-fsharp-mode"
   :depends (jsonrpc))
